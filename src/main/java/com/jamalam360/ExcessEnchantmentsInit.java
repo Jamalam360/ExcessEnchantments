035548c0b6c294348e2bb0f63dde783c104c76b7
Jamalam360 <jamesellison2006@gmail.com>
2021-03-31 14:45:22 +0100

    -Removed lots of unused imports

--- src/main/java/com/jamalam360/ExcessEnchantmentsInit.java
+++ src/main/java/com/jamalam360/ExcessEnchantmentsInit.java
@@ -1,19 +1,7 @@
 package com.jamalam360;
 
-import com.jamalam360.enchantments.armor.FireThornsEnchantment;
-import com.jamalam360.enchantments.armor.LastStandEnchantment;
-import com.jamalam360.enchantments.armor.MarathonEnchantment;
-import com.jamalam360.enchantments.armor.SwiftEnchantment;
-import com.jamalam360.enchantments.curse.HungerCurseEnchantment;
-import com.jamalam360.enchantments.curse.SlipperyCurseEnchantment;
-import com.jamalam360.enchantments.curse.BluntCurseEnchantment;
-import com.jamalam360.enchantments.curse.SnowCurseEnchantment;
-import com.jamalam360.enchantments.weapon.*;
 import com.jamalam360.util.EnchantmentRegistry;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
